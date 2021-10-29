01e18675c520b8ba88e4acf0a9ff7761ec5a883e
nguyenducthang <nguyenducthang231295@gmail.com>
2021-10-29 17:42:21 +0700

    init commit

---
+++ Doing game1/ChaiBeer.js
@@ -0,0 +1,67 @@
+class ChaiBeer {
+    constructor() {
+        this.x = 150;
+        this.y = 0;
+        this.speed = 4; //tốc độ ban đầu
+    }
+
+    init() {
+        let positions = [78, 178, 278, 437];// tọa độ x xuất hiện bia
+        this.x = positions[this.getRandomInt(0, 3)]; //lấy trong mảng position phần tử từ 0 đến 3
+
+    }
+
+    getRandomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;//tạo hàm lấy số ngẫu nhiên
+    }
+
+
+    update() {
+        if (this.y <= 380) {
+            this.y += (3 + this.speed);
+        }
+        this.checkInThungBeer();
+    }
+
+    checkInThungBeer() {
+
+        if (this.y + 50 >= 410) {
+            if (
+                (this.x > thungBeer.x) &&
+                (this.x < (thungBeer.x + thungBeer.width))
+            ) {
+                this.y = 0;
+                score += 1;
+                this.speed += 2;
+                if (score > 10 && score % 5 === 0) {
+                    this.speed = 10
+                }
+                soundGetScore = new Audio('sound/ghidiem.wav')
+                soundGetScore.play()
+                this.init();
+            } else {
+                 this.y = 0;
+                nhacNen.pause();
+                soundRestart.pause();
+                soundEndGame = new Audio('sound/laugh.wav')
+                soundEndGame.play()
+                alert('game over');
+                getName();
+                getPoint();
+                score = 0;
+                this.speed = 0;
+                drawGameOver();
+                clearSky();
+                clearInterval(timerId);
+
+            }
+        }
+
+    }
+
+    render(canvas) {
+        let ctx = canvas.getContext('2d');
+        ctx.drawImage(beerBottleImg, this.x, this.y, 30, 100);
+    }
+
+}
---
+++ Doing game1/ThungBeer.js
@@ -0,0 +1,23 @@
+class ThungBeer {
+    constructor(width, height) {
+        this.x = 100;
+        this.y = 470;
+        this.width = width;
+        this.height = height;
+    }
+
+    init(canvas) {
+        canvas.addEventListener('mousemove', function (event) {
+            processMouseMove(event);
+        })
+
+        function processMouseMove(event) {
+            let rect = canvas.getBoundingClientRect();
+            thungBeer.x = event.clientX - 50;
+        }
+    }
+    render(canvas) {
+        let ctx = canvas.getContext('2d');
+        ctx.drawImage(thungBeerImg, this.x, this.y, this.width, this.height)
+    }
+}
